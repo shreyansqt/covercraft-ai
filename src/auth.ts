25eca54339aaf9f8181899dfb32845bf980e5075
Shreyans Jain <shreyansqt@gmail.com>
2024-11-27 21:00:25 +0100

    add SupabaseAdapter to auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,10 @@
+import { SupabaseAdapter } from "@auth/supabase-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_SERVICE_ROLE_KEY) {
+  throw new Error("Missing Supabase URL or Service Role Key");
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
@@ -9,4 +14,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return !!auth;
     },
   },
+  adapter: SupabaseAdapter({
+    url: process.env.SUPABASE_URL,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
+  }),
 });
