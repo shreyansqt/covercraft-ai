7b30528e4635e78c8e0441635c9cf03cadcd668d
Shreyans Jain <shreyansqt@gmail.com>
2024-11-28 13:41:03 +0100

    add prisma

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,7 @@
-import { SupabaseAdapter } from "@auth/supabase-adapter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { prisma } from "./prisma";
 
 if (!process.env.SUPABASE_URL || !process.env.SUPABASE_SERVICE_ROLE_KEY) {
   throw new Error("Missing Supabase URL or Service Role Key");
@@ -14,8 +15,5 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return !!auth;
     },
   },
-  adapter: SupabaseAdapter({
-    url: process.env.SUPABASE_URL,
-    secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
-  }),
+  adapter: PrismaAdapter(prisma),
 });
